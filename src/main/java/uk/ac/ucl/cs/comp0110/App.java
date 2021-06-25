0da5d702bbec123e203ded69d127927160842998
arzhtong@outlook.com <arzhtong@outlook.com>
2021-06-25 20:33:14 +0100

    Add initial project files

---
+++ src/main/java/uk/ac/ucl/cs/comp0110/App.java
@@ -0,0 +1,13 @@
+package uk.ac.ucl.cs.comp0110;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
