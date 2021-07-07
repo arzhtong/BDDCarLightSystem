a3313e0789f7883a08655619b5eb24ae7840b95a
arzhtong@outlook.com <arzhtong@outlook.com>
2021-07-07 02:08:56 +0100

    Add steps for ELS-12 - Hazard Warning Light

--- src/test/java/uk/ac/ucl/cs/comp0110/HazardWarningLight_StepDefinitions.java
+++ src/test/java/uk/ac/ucl/cs/comp0110/HazardWarningLight_StepDefinitions.java
@@ -28,6 +28,32 @@ public class HazardWarningLight_StepDefinitions {
         Assert.assertEquals(car.getLengthOfHazardCycle(),1);
     }
 
+    @When("the hazard warning light switch is deactivated")
+    public void the_hazard_warning_light_switch_is_deactivated() {
+        car.setHazardSwitch(false);
+    }
+
+    @When("pitarm is in direction blinking left")
+    public void pitarm_is_in_direction_blinking_left() {
+        car.setPitmanArmPosition(PitmanArmPosition.DOWNWARD7);
+    }
+
+    @Then("the direction blinking cycle for the left indicator should start")
+    public void the_direction_blinking_cycle_for_the_left_indicator_should_start() {
+        Assert.assertEquals(car.getBlinkingState("Left"),Blinking.FLASHING);
+    }
+
+    @When("pitarm is in direction blinking right")
+    public void pitarm_is_in_direction_blinking_right() {
+        car.setPitmanArmPosition(PitmanArmPosition.UPWARD7);
+    }
+
+    @Then("the direction blinking cycle for the right indicator should start")
+    public void the_direction_blinking_cycle_for_the_right_indicator_should_start() {
+        Assert.assertEquals(car.getBlinkingState("Right"),Blinking.FLASHING);
+    }
+
+
 
 
 
